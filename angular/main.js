e7e35ec43e0a3d99b9ebc9437d67f6cc7de2b143
Shu Song <shusong@shusong-air.local>
2014-03-18 16:51:06 -0400

    new starting point

--- angular/main.js
+++ angular/main.js
@@ -1,28 +1,7 @@
-require.config({
-    paths: {
-        angular: 'bower_components/angular/angular',
-        'angular-route': 'bower_components/angular-route/angular-route'
-    },
-    shim: {
-        angular: {
-            exports: 'angular'
-        },
-        'angular-route': {
-            deps: ['angular']
-        }
-    }
-});
+var module = angular.module('app', []);
 
-require(['angular', 'angular-route'], function(angular) {
-    var module = angular.module('app', ['ngRoute']);
+module.controller('ctrl', ['$scope', function($scope) {
+    $scope.foo = 'hello world!';
 
-    module.controller('ctrl', ['$scope', function($scope) {
-        $scope.foo = 'hello world!';
-    }]);
 
-    module.config(function() {
-
-    });
-
-    angular.bootstrap(document, [module.name]);
-});
+}]);
