f059013db102c5d99c4d5ff7dfc4aa40f018e596
Shu Song <shusong@shusong-air.local>
2014-03-19 14:29:14 -0400

    added end of the presentation

--- angular/main.js
+++ angular/main.js
@@ -1,5 +1,47 @@
 var module = angular.module('app', []);
 
-module.controller('ctrl', ['$scope', function($scope) {
-    $scope.foo = 'hello world!';
+module.controller('ctrl', ['$scope', 'svc', function($scope, svc) {
+    $scope.foo = 'good bye world!';
+
+    $scope.alertMe = function(text) {
+        alert('button pressed with text: ' + text);
+    }
+
+    $scope.increment = function(x) {
+        svc.increment(x);
+    }
+
+    $scope.counter = function() {
+        return svc.counter;
+    }
 }]);
+
+module.controller('ctrl2', ['$scope', 'svc', function($scope, svc) {
+    $scope.counter = function() {
+        return svc.counter;
+    }
+}]);
+
+module.service('svc', function() {
+    this.counter = 0;
+    this.increment = function(x) {
+        x = parseInt(x) || 1;
+        this.counter += x;
+    }
+});
+
+module.directive('yoRedParagraph', function() {
+    return {
+        restrict: 'AE',
+        transclude: true,
+        replace: true,
+        scope: true,
+        template: '<div><p ng-click="clicked()" ng-class="{red: isRed}" ng-transclude></p></div>',
+        controller: ['$scope', function($scope) {
+            $scope.clicked = function() {
+                $scope.isRed = !$scope.isRed;
+                // alert('I was pressed');
+            };          
+        }]
+    };
+});
